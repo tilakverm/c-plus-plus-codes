eda98883852f075572e373e9875b69ce955ee54e
tilakverma225@gmail.com <tilakverma225@gmail.com>
2024-04-15 23:04:24 +0530

    sum of 2 num

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int x,y,sum;
+    cout<<"enter x :\n";
+    cin>>x;
+    cout<<"enter y :\n";
+    cin>>y;
+    sum=x+y;
+    cout<<"The sum is : "<<sum;
+    
+
+}
