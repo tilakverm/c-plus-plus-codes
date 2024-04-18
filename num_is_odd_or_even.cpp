318eb1c45a35e2244773c6d5c47db3bd2aad5d3b
tilakverma225@gmail.com <tilakverma225@gmail.com>
2024-04-18 21:27:53 +0530

    num_is_odd_or_even

---
+++ num_is_odd_or_even.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number\n";
+    cin>>n;
+   if(n%2==0){
+      cout<<"number is even";
+   }
+   else{
+    cout<<"number is odd";
+   }
+}
